6f276a6d4829702561879172eff9b50a845476c4
vmagnin <vincent.magnin@libertysurf.fr>
2022-11-23 21:58:39 +0100

    test/main.f90 updated

--- test/main.f90
+++ test/main.f90
@@ -10,10 +10,10 @@ program main
 
     implicit none
 
-    print *, "A4", get_MIDI_note("A4")
-    print *, "G9", get_MIDI_note("G9")
-    print *, "C0", get_MIDI_note("C0")
-    print *, "D#3", get_MIDI_note("D#3")
-    print *, "Eb6", get_MIDI_note("Eb6")
-    print *, get_MIDI_note(trim(HEXATONIC_BLUES_SCALE(1))//"0")
+    print *, "A4  is 69: ", get_MIDI_note("A4")
+    print *, "G9  is 127:", get_MIDI_note("G9")
+    print *, "C0  is 12: ", get_MIDI_note("C0")
+    print *, "D#3 is 51: ", get_MIDI_note("D#3")
+    print *, "Eb6 is 87: ", get_MIDI_note("Eb6")
+    print *, "C0  is 12: ", get_MIDI_note(trim(HEXATONIC_BLUES_SCALE(1))//"0")
 end program main
