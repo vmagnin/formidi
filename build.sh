f7190d42631378eb75e002d8c9a2003fb1a6e527
vmagnin <vincent.magnin@libertysurf.fr>
2024-06-03 22:34:23 +0200

    build.sh updated with latest changes

--- build.sh
+++ build.sh
@@ -16,15 +16,15 @@ fi
 rm -f *.mod
 
 if [ "${FC}" = "ifx" ]; then
-  ifx -warn -stand f18 src/formidi.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo1.f90 -o build/demo1.out
-  ifx -warn -stand f18 src/formidi.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo2.f90 -o build/demo2.out
-  ifx -warn -stand f18 src/formidi.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo3.f90 -o build/demo3.out
-  ifx -warn -stand f18 src/formidi.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo4.f90 -o build/demo4.out
+  ifx -warn -stand f18 src/formidi.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/third_kind.f90 -o build/third_kind.out
+  ifx -warn -stand f18 src/formidi.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/canon.f90 -o build/canon.out
+  ifx -warn -stand f18 src/formidi.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/blues.f90 -o build/blues.out
+  ifx -warn -stand f18 src/formidi.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90 example/circle_of_fifths.f90 -o build/circle_of_fifths.out
 else
-  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo1.f90 -o build/demo1.out
-  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo2.f90 -o build/demo2.out
-  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo3.f90 -o build/demo3.out
-  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/demo4.f90 -o build/demo4.out
+  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music_common.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/third_kind.f90 -o build/third_kind.out
+  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music_common.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/canon.f90 -o build/canon.out
+  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music_common.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/blues.f90 -o build/blues.out
+  "${FC}" -Wall -Wextra -pedantic -std=f2018 src/formidi.f90 src/music_common.f90 src/music.f90  src/GM_instruments.f90  src/MIDI_control_changes.f90 example/circle_of_fifths.f90 -o build/circle_of_fifths.out
 fi
 
 rm -f *.mod
