5220c873229ef84a8d24fc6661133a263cb1c335
vmagnin <vincent.magnin@libertysurf.fr>
2024-06-11 21:21:17 +0200

    example/canon.f90: arrays indexing now starts at 1

--- example/canon.f90
+++ example/canon.f90
@@ -2,7 +2,7 @@
 !          algorithmic music
 ! License GPL-3.0-or-later
 ! Vincent Magnin
-! Last modifications: 2024-06-10
+! Last modifications: 2024-06-11
 
 ! Based on the first measures of Pachelbel's Canon
 ! https://en.wikipedia.org/wiki/Pachelbel%27s_Canon
@@ -16,14 +16,14 @@ program canon
     implicit none
     type(MIDI_file) :: midi
     ! Notes of the bass and theme:
-    character(3), parameter :: bass(0:7) =  ["D3 ","A2 ","B2 ","F#2","G2 ", &
+    character(3), parameter :: bass(1:8) =  ["D3 ","A2 ","B2 ","F#2","G2 ", &
                                             &"D2 ","G2 ","A2 " ]
-    character(3), parameter :: theme(0:15) = [ "F#5","E5 ","D5 ","C#5", &
+    character(3), parameter :: theme(1:16) = [ "F#5","E5 ","D5 ","C#5", &
                                                 & "B4 ","A4 ","B4 ","C#5", &
                                                 & "D5 ","C#5","B4 ","A4 ", &
                                                 & "G4 ","F#4","G4 ","E4 " ]
     ! List of General MIDI instruments to use sequentially:
-    integer(int8), parameter :: instrument(0:16) = [ 40, 41, 42, 44, 45, 48,&
+    integer(int8), parameter :: instrument(1:17) = [ 40, 41, 42, 44, 45, 48,&
                                 & 49, 51, 52, 89, 90, 91, 92, 94, 95, 99, 100 ]
     integer(int8) :: track
     character(13) :: track_name
@@ -44,7 +44,7 @@ program canon
     call midi%Program_Change(channel=0_int8, instrument=String_Ensemble_1)
 
     do j = 1, 30
-        do i = 0, 7
+        do i = 1, 8
             call midi%play_note(channel=0_int8, note=get_MIDI_note(bass(i)), velocity=64_int8, duration=quarter_note)
         end do
     end do
@@ -58,12 +58,12 @@ program canon
         call midi%Control_Change(channel=track, type=Effects_1_Depth, ctl_value=64_int8)  ! Reverb
         call midi%play_note(channel=track, note=0_int8, velocity=0_int8, duration=8*quarter_note*(track - 2))
 
-        do j = 0, 14
+        do j = 1, 15
             ! Let's change the instrument to add some variations:
             call midi%Program_Change(channel=track, &
                                     & instrument=int(instrument((track - 3) + j), int8))
             ! Let's play the theme:
-            do i = 0, 15
+            do i = 1, 16
                 call midi%play_note(channel=track, note=get_MIDI_note(theme(i)), velocity=64_int8, duration=quarter_note)
             end do
         end do
