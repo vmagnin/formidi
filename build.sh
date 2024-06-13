08e7370918c72fba8b9efd233806cdc15f994511
vmagnin <vincent.magnin@libertysurf.fr>
2024-06-13 20:08:07 +0200

    build.sh: fixes the missing MIDI_file_class

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Vincent Magnin
-# Last modification: 2024-06-08
+# Last modification: 2024-06-13
 
 # For a safer script:
 set -eu
@@ -25,12 +25,12 @@ else
 fi
 
 # Compiling modules:
-"${FC}" ${flags} -c src/formidi.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90
+"${FC}" ${flags} -c src/MIDI_file_class.f90 src/music_common.f90 src/music.f90 src/GM_instruments.f90  src/MIDI_control_changes.f90
 
 # Compiling examples:
 for file in "third_kind" "canon" "blues" "circle_of_fifths" "la_folia" ; do
   echo "${file}"
-  "${FC}" ${flags} formidi.o music_common.o music.o  GM_instruments.o  MIDI_control_changes.o example/${file}.f90 -o build/${file}.out
+  "${FC}" ${flags} MIDI_file_class.o music_common.o music.o  GM_instruments.o  MIDI_control_changes.o example/${file}.f90 -o build/${file}.out
 done
 
 # Cleanup to avoid any problem with fpm or another compiler:
